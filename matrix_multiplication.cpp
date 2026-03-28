1f812f5cccbce5d2d70a7208f62d5be19b032875
gar1998 <gareevadel262825@yandex.ru>
2026-03-28 22:25:23 +0400

    Проверка отображения отчёта

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 #include <chrono>
+#include <algorithm>
+#include <omp.h>
 
 using namespace std;
 
@@ -14,9 +16,7 @@ vector<long long> read_matrix(const string& filename, size_t& size)
         std::cerr << "Error: Could not open file " << filename << endl;
         exit(1);
     }
-
     file >> size;
-
     vector<long long> matrix_value(size * size);
     for (int i = 0; i < size * size; ++i)
     {
@@ -29,7 +29,7 @@ vector<long long> read_matrix(const string& filename, size_t& size)
     return matrix_value;
 }
 
-void write_matrix(const string& filename, const vector<long long>& matrix_flat, size_t& size, auto& duration)
+void write_matrix(const string& filename, const vector<long long>& matrix_flat, size_t& size)
 {
     ofstream file(filename);
     if (!file.is_open())
@@ -37,7 +37,6 @@ void write_matrix(const string& filename, const vector<long long>& matrix_flat, 
         cerr << "Error: Could not create/open file " << filename << endl;
         exit(1);
     }
-
     file << size << endl;
     for (size_t i = 0; i < size; ++i)
     {
@@ -45,15 +44,28 @@ void write_matrix(const string& filename, const vector<long long>& matrix_flat, 
         {
             file << matrix_flat[i * size + j] << (j == size - 1 ? "" : " ");
         }
-
         file << endl;
     }
+}
 
-    file << "Время выполнения задачи: " << duration << endl;
-    file << "Объём задачи: " << size << " - порядок матриц";
 
+void write(const string& filename, const vector<double>& times, const vector<int>& threads)
+{
+    ofstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Error: Could not create/open file " << filename << endl;
+        exit(1);
+    }
+    file << "Количество потоков:" << " " << "Время выполнения" << "\n";
+
+    for (int i = 0; i < times.size(); ++i)
+    {
+        file << threads[i] << " " << times[i] << "\n";
+    }
 
 }
+
 
 int main()
 {
@@ -67,37 +79,58 @@ int main()
 
     if (A_matrix_size != B_matrix_size)
     {
-        cerr << "Error: Matrix dimensions do not match (" << A_matrix_size << "*" << A_matrix_size << " and " << B_matrix_size << "*" << B_matrix_size << ")" << endl;
+        cerr << "Error: Matrix dimensions do not match" << endl;
         return 1;
     }
 
     size_t N = A_matrix_size;
+    string statistics = "C:\\Users\\Адель\\Desktop\\lab-1\\Data\\statistics_data_" + to_string(N) + ".txt";
     vector<long long> result_matrix(N * N, 0LL);
 
-    auto start_time = chrono::high_resolution_clock::now();
-
-    for (size_t i = 0; i < N; ++i)
+    int num_threads = 1; 
+    vector<double> times = {};
+    vector<int> threads = {};
+  
+    while (num_threads<=16)
     {
-        for (size_t j = 0; j < N; ++j)
+        omp_set_num_threads(num_threads);
+
+        cout << "Running with " << num_threads << " threads..." << endl;
+
+        auto start_time = chrono::high_resolution_clock::now();
+
+        #pragma omp parallel for
+        for (int i = 0; i < (int)N; ++i)
         {
-            long long sum = 0LL;
             for (size_t k = 0; k < N; ++k)
             {
-                sum += A_matrix_values[i * N + k] * B_matrix_values[k * N + j];
-            }
+                long long temp = A_matrix_values[i * N + k];
 
-            result_matrix[i * N + j] = sum;
+                for (size_t j = 0; j < N; ++j)
+                {
+                    result_matrix[i * N + j] += temp * B_matrix_values[k * N + j];
+                }
+            }
         }
+
+        auto end_time = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> duration = end_time - start_time;
+
+        cout << "Task size: " << N << "x" << N << endl;
+        cout << "Execution time (ms): " << duration.count() << endl;
+        cout << "\n";
+
+        times.push_back(duration.count());
+        threads.push_back(num_threads);
+
+        if (num_threads!=16)
+            fill(result_matrix.begin(), result_matrix.end(), 0LL);
+
+        num_threads++;
     }
 
-    auto end_time = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> duration = end_time - start_time;
-
-    cout << "Task size: " << N << endl;
-    cout << "Execution time (seconds): " << duration << endl;
-    cout << "Result written to: " << result_file << endl;
-
-    write_matrix(result_file, result_matrix, N, duration);
+    write(statistics, times, threads);
+    write_matrix(result_file, result_matrix, N);
 
     return 0;
 }
